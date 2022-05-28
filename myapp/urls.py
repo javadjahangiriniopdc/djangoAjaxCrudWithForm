401ac75e6c51243f40f716f352437649bbbfa66a
drjj007 <javad.jahangiri.niopdc@gmail.com>
2022-05-28 02:08:56 -0700

    add myAjax and need url

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('save/', views.save_data, name='save'),
+    path('delete/', views.delete_data, name='delete'),
+    path('edit/', views.edit_data, name='edit'),
 ]
