cffcdb9f75b60ba40899dae60a81d9d970c3c8c7
drjj007 <javad.jahangiri.niopdc@gmail.com>
2022-05-28 01:41:13 -0700

    add student model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    course = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
